3332ace87760b48670d0147335720ea2fa3714a8
Michael Sinai <michael.sinai@vts.com>
2017-04-26 15:42:45 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |x, y|
+    x.length <=> y.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |array_item|
+      array_item.insert(2, "$")
+      array_item.delete!(array_item[3])
+  end
+  array
+end
+
+
+def find_a(array)
+  array.select {|array_item| array_item.start_with?('a')}
+end
+
+def sum_array(array)
+  array.inject {|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.map! do |array_item|
+    array_item + "s"
+  end
+  array[1].chop!
+  array
+
+end
